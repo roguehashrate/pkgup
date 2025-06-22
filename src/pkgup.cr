ba8d862e1ae4e8f2f320bfeeac98caa5d8f228fe
roguehashrate <roguehashrate@gmail.com>
2025-06-22 08:30:51 -0500

    Added Non-Interactive mode and a few clean ups

--- src/pkgup.cr
+++ src/pkgup.cr
@@ -9,7 +9,33 @@ module Pkgup
 
   VERSION = "0.1.0"
 
+  def self.update_sys(auth_cmd : String, pkg_man : String)
+    system("#{auth_cmd} #{pkg_man} update && #{auth_cmd} #{pkg_man} upgrade -y")
+    puts "#{GREEN}System package update complete!#{RESET}"
+  end
+
+  def self.update_flat
+      system("flatpak update -y")
+      puts "#{GREEN}Flatpak update complete!#{RESET}"
+  end
+
   def self.run
+    
+      if ARGV.includes?("--version") || ARGV.includes?("-v")
+    puts "Pkgup version #{VERSION}"
+    exit
+  elsif ARGV.includes?("--help") || ARGV.includes?("-h")
+    puts <<-HELP
+    Usage: pkgup [option]
+      sys      Update system packages
+      flat     Update Flatpak packages
+      both     Update both system and Flatpak
+      -v       Show version
+      -h       Show help
+    HELP
+    exit
+  end
+
     auth_cmd = if system("which doas > /dev/null 2>&1")
       "doas"
     else
@@ -22,22 +48,24 @@ module Pkgup
       "apt"
     end
 
-    print "#{BLUE}Would you like to update [sys]pkgs, [flatpak]pkgs, or [both]: #{RESET}"
-    user_choice = gets.to_s.strip.downcase
+    user_choice = ARGV[0]?.try(&.strip.downcase)
 
+    if user_choice.nil? || user_choice.empty?
+      print "#{BLUE}Would you like to update [sys] pkgs, [flat]pak pkgs, or [both]: #{RESET}"
+      user_choice = gets.try(&.strip.downcase) || ""
+    end
+    
     case user_choice
     when "sys"
       puts "#{YELLOW}Updating System Programs...#{RESET}"
-      system("#{auth_cmd} #{pkg_man} update && #{auth_cmd} #{pkg_man} upgrade -y")
-      puts "#{GREEN}System update complete!#{RESET}"
-    when "flatpak"
+      update_sys(auth_cmd, pkg_man)
+    when "flat"
       puts "#{YELLOW}Updating Flatpak Programs...#{RESET}"
-      system("flatpak update -y")
-      puts "#{GREEN}Flatpak update complete!#{RESET}"
+      update_flat
     when "both"
       puts "#{YELLOW}Updating System and Flatpak Programs...#{RESET}"
-      system("#{auth_cmd} #{pkg_man} update && #{auth_cmd} #{pkg_man} upgrade -y")
-      system("flatpak update -y")
+      update_sys(auth_cmd, pkg_man)
+      update_flat
       puts "#{GREEN}All updates complete!#{RESET}"
     else
       puts "#{RED}Invalid option. Please enter 'sys', 'flatpak', or 'both'.#{RESET}"
